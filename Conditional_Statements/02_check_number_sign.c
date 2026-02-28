78fd07c04e016f91fd3da5d0130bf25bdf8b0602
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-02-28 20:51:15 +0500

    Add program to check if a number is positive, negative, or zero
    Implement number sign checking logic.

---
+++ Conditional_Statements/02_check_number_sign.c
@@ -0,0 +1,17 @@
+// Check whether a number is positive, negative, or zero
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num > 0){
+    printf("Positive number.\n");
+    }
+    else if(num < 0){
+    printf("Negative number.\n");
+    }
+    else {
+    printf("Your number is zero.\n");
+    }
+    return 0;
+}
