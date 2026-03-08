274942d2529952124d78d5c7c73dfcfe4d20a44f
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-03-08 20:36:00 +0500

    Refactor even/odd checker with input validation
    Updated input prompt and added input validation for numeric values.

--- Conditional_Statements/01_check_even_or_odd.cpp
+++ Conditional_Statements/01_check_even_or_odd.cpp
@@ -1,14 +1,17 @@
-// Program to check whether a number is even or odd
+// 1. Check whether a number is even or odd
 #include<stdio.h>
+
 int main(){
     int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if(num % 2 == 0){
-    printf("%d is an even number\n", num);    }
-    else {
-    printf("%d is an odd number ", num);
-
+    printf("Enter your number: ");
+    if(scanf("%d", &num) != 1){
+        printf("Invalid input! Please enter a numeric value.");
+        return 1;
+    }
+    if(num%2==0){
+        printf("Your number %d is even", num);
+    } else{
+        printf("Your number %d is Odd", num);
     }
     return 0;
 }
