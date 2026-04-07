69870bc4e402520fb35096d6b1dcb382c144dad8
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-04-07 21:06:51 +0500

    Add array sorting program in ascending order
    Implement a program to sort an array in ascending order.

---
+++ Arrays/06_sort_array_ascending.c
@@ -0,0 +1,27 @@
+// Sort an array (ascending order) 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+  printf("Sorted array (ascending): ");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
