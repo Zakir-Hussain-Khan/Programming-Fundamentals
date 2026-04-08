add1a07cc9894cc69017d0b8976cac38facae62e
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-04-08 22:46:39 +0500

    Add program to sum two 2D matrices

---
+++ Arrays/08_add_two_2d_matrices.c
@@ -0,0 +1,39 @@
+// Add two 2D matrices
+
+#include <stdio.h>
+
+int main() {
+    int rows = 2, cols = 2;
+    int A[2][2], B[2][2], sum[2][2];
+
+    printf("Enter elements of first matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
