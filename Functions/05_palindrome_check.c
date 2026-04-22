0f4b228b23db051c2eabb22b26e0a965aea2926b
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-04-22 19:47:59 +0500

    Add palindrome check function in C

---
+++ Functions/05_palindrome_check.c
@@ -0,0 +1,32 @@
+// Function to check palindrome
+
+#include <stdio.h>
+
+int isPalindrome(int n) {
+    int original = n;
+    int reversed = 0;
+
+    while (n != 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+
+    if (original == reversed)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num)) {
+        printf("%d is a palindrome.\n", num);
+    } else {
+        printf("%d is not a palindrome.\n", num);
+    }
+    
+    return 0;
+}
