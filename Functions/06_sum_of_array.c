70c8ab828f14c864bb7fc2bdcb9d525c0ffb2535
Zakir Hussain <161231538+Zakir-Hussain-Khan@users.noreply.github.com>
2026-04-23 23:02:24 +0500

    Add function to calculate sum of array elements

---
+++ Functions/06_sum_of_array.c
@@ -0,0 +1,27 @@
+// Function to calculate sum of array elements
+
+#include <stdio.h>
+
+int arraySum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Sum of array elements is %d\n", arraySum(arr, n));
+
+    return 0;
+}
